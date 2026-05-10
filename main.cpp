261845fd21dd902db4529386cdb953bce0098044
lingqiancariad <lingqian.meng@cariad.technology>
2026-05-10 20:23:52 +0200

    Create main()

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "--- Starting Automotive Data Acquisition Demo ---" << std::endl;
+
+    // todo...
+    
+    std::cout << "Demo complete." << std::endl;
+    return 0;
+}
